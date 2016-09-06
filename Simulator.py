81409ac2afb639386a0b3bdef0832b06009e2b92
Giorgi Nadiradze <d.alistarh@gmail.com>
2016-09-06 07:51:34 -0400

    smallchange

--- Simulator.py
+++ Simulator.py
@@ -6,8 +6,8 @@ import shlex
 
 NREPEATS = 10  #number of times to call Dijkstra in order to measure average time
 seed = 0
-prob = 0 #probability of having an edge, if 0 use social networsk graph
-beta = 0 #probability of using two random queues
+prob = 0.0 #probability of having an edge, if 0 use social networsk graph
+betaValues = [0.0, 0.5]  #probability of using two random queues
 
 #build kpqueue
 
@@ -16,7 +16,7 @@ args = shlex.split(cmd)
 
 subprocess.call(args)
 
-def RunDijkstra(version, p):
+def RunDijkstra(version, p, beta):
 	
 	time = []
 	
@@ -49,16 +49,27 @@ QueueVersions = ["dlsm", "globallock", "klsm", "multiq"]
 
 f = open('RunTimes.txt', 'w')
 
-for i in range(0, 4):
-	time = RunDijkstra(QueueVersions[i], prob) 
+for i in range(0, 3):
+	time = RunDijkstra(QueueVersions[i], prob, 0.0) 
 	j = 1 
 	k = 0
 	while j <= 32:
 		f.write(QueueVersions[i] + ' ' + str(j) + ' ' + str(time[k]) + '\n')
 		j *= 2
 		k += 1
-		
-time = RunDijkstra("sequential", prob)
+
+for beta in betaValues:		
+        time = RunDijkstra(QueueVersions[3], prob, beta)
+        j = 1
+        k = 0
+        while j <= 32:
+                f.write(QueueVersions[3] + ' ' + str(beta)+' ' + str(j) + ' ' + str(time[k]) + '\n')
+                j *= 2
+                k += 1
+ 
+
+
+time = RunDijkstra("sequential", prob, 0.0)
 f.write('Sequential 1 '+str(time[0])+'\n')
 
 
