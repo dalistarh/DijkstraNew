e4a26cb0de999ca715e118ba349b2d45a5b71add
Giorgi128 <gionadira@gmail.com>
2016-10-18 17:26:04 +0200

    change4

--- kpqueue/src/sequential_lsm/lsm.h
+++ kpqueue/src/sequential_lsm/lsm.h
@@ -39,7 +39,7 @@ public:
 
     void insert(const T &k, const T &v);
     bool delete_min(T &v);
-    bool delete_min2(V &value, const int &thread_id) {}
+    bool delete_min2(T &value, const int &thread_id) {}
     
 
     void clear();
