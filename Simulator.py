1d85cf2bddfe3e8c6aa1f4598de7cd4d5ca1efbb
Giorgi128 <gionadira@gmail.com>
2016-10-20 15:58:43 +0200

    all

--- Simulator.py
+++ Simulator.py
@@ -6,25 +6,29 @@ import shlex
 
 NREPEATS = 10  #number of times to call Dijkstra in order to measure average time
 seed = 0
-prob = 0.0 #probability of having an edge, if 0 use social networsk graph
-betaValues = [0.0, 0.5]  #probability of using two random queues
+prob = 0.1 #probability of having an edge, if 0 use social networsk graph
+betaValues = [1, 5, 15, 20]  #probability of using two random queues
+nvertices = 10000;
+nthreads = 64
+nqueues = 64
+version = "multiq"
 
 #build kpqueue
-
 cmd = "make -C kpqueue"
 args = shlex.split(cmd)
-
 subprocess.call(args)
 
-def RunDijkstra(version, p, beta):
+def RunDijkstra(beta):
 	
-	time = []
+		#run concurrent Dijkstra with betanthreads #threads
 	
-	nthreads = 1
-	while nthreads <= 32:
-		#run concurrent Dijkstra with nthreads #threads
-	
-		cmd = "./kpqueue/build/src/bench/shortest_paths -m 4039 -n " +str(nthreads)+" -p "+str(p)+" -s "+str(seed)+" -b "+str(beta)+" "+version;
+		cmd = "./kpqueue/build/src/bench/shortest_paths"+" -m " + str(nvertices)+ \
+                                                         " -n " +str(nthreads)+  \
+                                                         " -p " + str(prob)+        \
+                                                         " -s "+str(seed)+      \
+                                                         " -b "+str(beta)+          \
+                                                         " -q "+str(nqueues)+       \
+                                                         " " + version  
 		args = shlex.split(cmd)
 
 		t = 0.0
@@ -33,44 +37,26 @@ def RunDijkstra(version, p, beta):
 		
 			ans = subprocess.check_output(args)
 
-			st = subprocess.check_output(args);
+			st = subprocess.check_output(args)
 			st = st[:-2]
 			
 			t  += float(st)
 			
 			
-		time.append(t / NREPEATS)
-		nthreads *= 2
-		if version == "sequential": break
-	return time
+		return t / NREPEATS
 		
-
-QueueVersions = ["dlsm", "globallock", "klsm", "multiq"]
 
 f = open('RunTimes.txt', 'w')
 
-for i in range(0, 3):
-	time = RunDijkstra(QueueVersions[i], prob, 0.0) 
-	j = 1 
-	k = 0
-	while j <= 32:
-		f.write(QueueVersions[i] + ' ' + str(j) + ' ' + str(time[k]) + '\n')
-		j *= 2
-		k += 1
+f.write(str(nvertices)+" nodes;"+str(nthreads)+" threads;"+str(nqueues)+" queues;"+str(prob)+" edge probability;"+"\n");
 
 for beta in betaValues:		
-        time = RunDijkstra(QueueVersions[3], prob, beta)
-        j = 1
-        k = 0
-        while j <= 32:
-                f.write(QueueVersions[3] + ' ' + str(beta)+' ' + str(j) + ' ' + str(time[k]) + '\n')
-                j *= 2
-                k += 1
- 
+        time = RunDijkstra(beta)
+        f.write("time="+str(time)+";"+"beta="+str(beta)+"\n")
 
 
-time = RunDijkstra("sequential", prob, 0.0)
-f.write('Sequential 1 '+str(time[0])+'\n')
+
+
 
 
 
